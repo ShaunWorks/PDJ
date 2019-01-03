9a333cb021d38258109de8afb1f3eafc0f39c7c1
Torian <tharris@zyrobotics.com>
2019-01-03 14:09:23 -0500

    gif animation toggle

--- Javascript/app.js
+++ Javascript/app.js
@@ -10,16 +10,26 @@ let $grid = $('.grid').masonry({
   });
 
 $.ajax({
-    url: queryURL("joggers"),
+    url: queryURL("dog", GIPHY_APIKEY, 100),
     method: "GET"
 }).done(function (response) {
     console.log(response.data[0].images);
-    for (let i = 0; i < 25; i++) {
-        let holder = $("<div>").addClass("grid-item");
-        let image = $("<img>").attr("src", response.data[i].images.original_still.url);
+    // for (let i = 0; i < 25; i++) {
+    //     let holder = $("<div>").addClass("grid-item");
+    //     let image = $("<img>").attr("src", response.data[i].images.original_still.url);
+    //     holder.append(image);
+    //     gifs.push(holder);
+    // }
+    $.each(response.data, function(key, value) {
+        let holder = $("<div>").addClass("grid-item gif").attr("clicked", "false");
+        let image = $("<img>").attr({
+            "src": value.images.original_still.url,
+            "still": value.images.original_still.url,
+            "animated": value.images.original.url
+        });
         holder.append(image);
         gifs.push(holder);
-    }
+    })
     displayGifs();
 
 }).fail(function (response) {
@@ -36,11 +46,24 @@ function displayGifs()
       });
 }
 
-function queryURL (query, key)
+function queryURL (query, key, limit)
 {
-    return HOSTURL + `q=${query}&api_key=${key}`
+    return HOSTURL + `q=${query}&api_key=${key}&limit=${limit}`
 }
 
 $("#test").on("click", function(){
     $grid.masonry('layout');
 });
+
+$(".grid").on("click", ".gif", function() {
+    if($(this).attr("clicked") == "false")
+    {
+        $(this).find("img").attr("src", $(this).find("img").attr("animated"));
+        $(this).attr("clicked", "true");
+    }
+    else
+    {
+        $(this).find("img").attr("src", $(this).find("img").attr("still"));
+        $(this).attr("clicked", "false");
+    }
+})
