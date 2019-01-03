d531cfe0e36aa1758540641a1dfa57e1d8e3505e
Torian <tharris@zyrobotics.com>
2019-01-03 15:17:15 -0500

    scrolling

--- Javascript/app.js
+++ Javascript/app.js
@@ -2,39 +2,41 @@ const GIPHY_APIKEY = "hFBK0Y9nBlTuUIHFkoN2wZL0W9AzGWgV";
 const HOSTURL = "http://api.giphy.com/v1/gifs/search?";
 
 let gifs = [];
-var bool = true;
+let apiCalls = 0;
+
 let $grid = $('.grid').masonry({
     itemSelector: '.grid-item',
     percentPosition: true,
     columnWidth: '.grid-sizer'
   });
 
-$.ajax({
-    url: queryURL("dog", GIPHY_APIKEY, 100),
-    method: "GET"
-}).done(function (response) {
-    console.log(response.data[0].images);
-    // for (let i = 0; i < 25; i++) {
-    //     let holder = $("<div>").addClass("grid-item");
-    //     let image = $("<img>").attr("src", response.data[i].images.original_still.url);
-    //     holder.append(image);
-    //     gifs.push(holder);
-    // }
-    $.each(response.data, function(key, value) {
-        let holder = $("<div>").addClass("grid-item gif").attr("clicked", "false");
-        let image = $("<img>").attr({
-            "src": value.images.original_still.url,
-            "still": value.images.original_still.url,
-            "animated": value.images.original.url
-        });
-        holder.append(image);
-        gifs.push(holder);
-    })
-    displayGifs();
 
-}).fail(function (response) {
-    console.log(response);
-})
+function getGifs (query, key, limit) {
+    $.ajax({
+        url: HOSTURL + `q=${query}&api_key=${key}&limit=${limit}&offset=${apiCalls}`,
+        method: "GET"
+    }).done(function (response) {
+        console.log(response.data[0].images);
+        gifs = [];
+        $.each(response.data, function(key, value) {
+            let holder = $("<div>").addClass("grid-item gif").attr("clicked", "false");
+            let image = $("<img>").attr({
+                "src": value.images.original_still.url,
+                "still": value.images.original_still.url,
+                "animated": value.images.original.url
+            });
+            holder.append(image);
+            gifs.push(holder);
+        })
+        displayGifs();
+    
+    }).fail(function (response) {
+        console.log(response);
+    })
+
+    apiCalls += limit;
+}
+
 
 function displayGifs()
 {
@@ -67,3 +69,11 @@ $(".grid").on("click", ".gif", function() {
         $(this).attr("clicked", "false");
     }
 })
+
+$(window).on('scroll', function() {
+    if( $(window).scrollTop() + $(window).height() == $(document).height() ) {
+        getGifs("dog", GIPHY_APIKEY, 100);
+    }
+});
+
+getGifs("dog", GIPHY_APIKEY, 100);
